1b3b66d4f5ac77a93209bc692feb715719fc768d
Jhurds <georgejoseabaygar@gmail.com>
2024-04-13 16:50:13 +0800

    Adds config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigService } from './config/config.service';
 import { AuthenticationModule } from './authentication/authentication.module';
-import { ClerkModule } from './clerk/clerk.module';
+import config from './config/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.development.env',
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     AuthenticationModule,
-    ClerkModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,11 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT) || 3000,
   database: {
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  },
+  clerk: {
+    publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    secretKey: process.env.CLERK_POKEMON_KEY,
   },
 });
