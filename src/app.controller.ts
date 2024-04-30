16ed39032a969d32289f98b1fe1754f4eb3a75ba
Jhurds <georgejoseabaygar@gmail.com>
2024-04-30 15:28:32 +0800

    Adds healthcheck route and helloworld route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('helloworld')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('healthcheck')
+  HealthCheck(): string {
+    return this.appService.healthCheck();
   }
 }
