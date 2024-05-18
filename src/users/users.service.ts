d065d27cbd3d584398a577feb537289607d203f1
Jhurds <georgejoseabaygar@gmail.com>
2024-05-18 16:28:00 +0800

    Remove more unused imports

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
-import { User, UserDocument } from './entities/user.schema';
+import { User } from './entities/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
