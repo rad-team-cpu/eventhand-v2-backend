2ce6386b6c39827f140606b18c9bf3cfd72b8067
Jhurds <georgejoseabaygar@gmail.com>
2024-05-18 12:48:04 +0800

    Remove Length Validation for clerkId

--- src/users/dto/search-clerk.dto.ts
+++ src/users/dto/search-clerk.dto.ts
@@ -1,7 +1,6 @@
-import { IsString, Length } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class SearchClerkIdDto {
   @IsString()
-  @Length(1, 20)
   clerkId: string;
 }
