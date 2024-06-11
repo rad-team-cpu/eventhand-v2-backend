f59d7a3054d574a220bafbcae606600904c810e5
Jhurds <georgejoseabaygar@gmail.com>
2024-06-11 15:44:13 +0800

    remove conflict check

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,15 +4,16 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { stringify } from 'querystring';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    if (this.userModel.exists({ clerkId: createUserDto.clerkId })) {
-      throw new ConflictException();
-    }
+    // if (result) {
+    //   throw new ConflictException(`${result} already exists`);
+    // }
 
     return this.userModel.create(createUserDto);
   }
