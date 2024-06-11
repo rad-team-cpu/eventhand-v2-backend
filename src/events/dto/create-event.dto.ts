04335ac74e68dc3eae778158664ef7fa612fa67e
Jhurds <georgejoseabaygar@gmail.com>
2024-06-11 14:38:01 +0800

    Add create event dto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,18 @@
-export class CreateEventDto {}
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
+export class CreateEventDto {
+  @IsString()
+  userId: string;
+
+  @IsString()
+  clerkId: string;
+
+  @IsNumber()
+  attendees: number;
+
+  @IsDate()
+  Date: string;
+
+  @IsNumber()
+  budget: number;
+}
