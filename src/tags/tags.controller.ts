4d88c807a271127dcdad6766c40de7d591c0bb43
Jhurds <georgejoseabaygar@gmail.com>
2024-06-25 15:51:30 +0800

    Fix stuff

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { VendorTag } from './entities/vendor-tag.schema';
 
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
   @Post()
-  create(@Body() createTagDto: CreateTagDto) {
-    return this.tagsService.create(createTagDto);
+  async create(@Body() createTagDto: CreateTagDto): Promise<VendorTag> {
+    return await this.tagsService.create(createTagDto);
   }
 
   @Get()
