a2d33dd8a6afcd1beb0144834def6ca1197d977d
Jhurds <georgejoseabaygar@gmail.com>
2024-07-10 15:03:58 +0800

    Reformat

--- src/verification/verification.service.ts
+++ src/verification/verification.service.ts
@@ -1,29 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import { executablePath } from 'puppeteer';
-
 @Injectable()
 export class VerificationService {
   // constructor(private readonly puppeteer: puppeteer.Puppeteer) {}
   private browser: puppeteer.Browser;
 
-  async initializeBrowser(): Promise<void> {
-    const pathToExtension = require('path').join(__dirname, '2captcha-solver');
-    puppeteer.use(StealthPlugin());
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: [
-        `--disable-extensions-except=${pathToExtension}`,
-        `--load-extension=${pathToExtension}`,
-      ],
-      executablePath: executablePath(),
-    });
-  }
+  async initializeBrowser(): Promise<void> {}
 
   async closeBrowser(): Promise<void> {
     await this.browser.close();
   }
-
-  async;
 }
