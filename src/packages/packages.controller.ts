a7dc9851e737bc7b0756792de33d806426035e28
Jhurds <georgejoseabaygar@gmail.com>
2024-07-11 14:10:34 +0800

    Add crud routes to packages

--- src/packages/packages.controller.ts
+++ src/packages/packages.controller.ts
@@ -6,37 +6,43 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { PackagesService } from './packages.service';
 import { CreatePackageDto } from './dto/create-package.dto';
 import { UpdatePackageDto } from './dto/update-package.dto';
+import { Package } from './entities/package.schema';
+import { FilterQuery } from 'mongoose';
 
 @Controller('packages')
 export class PackagesController {
   constructor(private readonly packagesService: PackagesService) {}
 
   @Post()
-  create(@Body() createPackageDto: CreatePackageDto) {
+  async create(@Body() createPackageDto: CreatePackageDto): Promise<Package> {
     return this.packagesService.create(createPackageDto);
   }
 
   @Get()
-  findAll() {
-    return this.packagesService.findAll();
+  findAll(@Query() query?: FilterQuery<Package>): Promise<Package[]> {
+    return this.packagesService.findAll(query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.packagesService.findOne(+id);
+  findOne(@Param('id') id: string): Promise<Package> {
+    return this.packagesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePackageDto: UpdatePackageDto) {
-    return this.packagesService.update(+id, updatePackageDto);
+  update(
+    @Param('id') id: string,
+    @Body() updatePackageDto: UpdatePackageDto,
+  ): Promise<Package> {
+    return this.packagesService.update(id, updatePackageDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.packagesService.remove(+id);
+  remove(@Param('id') id: string): Promise<void> {
+    return this.packagesService.remove(id);
   }
 }
