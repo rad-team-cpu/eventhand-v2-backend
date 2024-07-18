b86f41f4d5614e01ba81482b851e96d0e668ff83
Jhurds <georgejoseabaygar@gmail.com>
2024-07-18 15:05:57 +0800

    Add Booking Schema

---
+++ src/booking/entities/booking.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Schema as MongooseSchema } from 'mongoose';
+import { Package } from 'src/packages/entities/package.schema';
+import { User } from 'src/users/entities/user.schema';
+import { Vendor } from 'src/vendors/entities/vendor.schema';
+
+export enum BookingStatus {
+  Pending = 'PENDING',
+  Confirmed = 'CONFIRMED',
+}
+
+@Schema({ timestamps: true })
+export class Booking {
+  @Prop({
+    required: true,
+    type: MongooseSchema.Types.ObjectId,
+    ref: Vendor.name,
+  })
+  vendorId: MongooseSchema.Types.ObjectId;
+
+  @Prop({ required: true, type: MongooseSchema.Types.ObjectId, ref: User.name })
+  userId: MongooseSchema.Types.ObjectId;
+
+  @Prop({
+    required: true,
+    type: MongooseSchema.Types.ObjectId,
+    ref: Package.name,
+  })
+  packageId: MongooseSchema.Types.ObjectId;
+
+  @Prop({ required: true })
+  bookingDate: Date;
+
+  @Prop({ required: true, default: BookingStatus.Pending })
+  bookingStatus: BookingStatus;
+}
