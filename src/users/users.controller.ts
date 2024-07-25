672675e818f3c212422a68ac5d2c60e9bdb4c5ea
DelgadoArlee <84315721+DelgadoArlee@users.noreply.github.com>
2024-07-25 11:25:10 +0800

    Update users.controller.ts
    Fix imports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Query,
+  NotFoundException
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
