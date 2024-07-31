69f6a3b97b05a48a67d0d77608be1f1e6ff0fc60
Jhurds <georgejoseabaygar@gmail.com>
2024-07-31 12:24:51 +0800

    Create new user

--- src/clerk/clerk.service.ts
+++ src/clerk/clerk.service.ts
@@ -2,6 +2,7 @@ import { WebhookEvent } from '@clerk/clerk-sdk-node';
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { Gender } from 'src/users/entities/user.schema';
 
 @Injectable()
 export class WebhookService {
@@ -10,31 +11,27 @@ export class WebhookService {
   async handleEvent(evt: WebhookEvent): Promise<string> {
     switch (evt.type) {
       case 'user.created': {
-        if (evt.data.unsafe_metadata.type === 'CLIENT') {
-          const {
-            id,
-            first_name,
-            last_name,
-            email_addresses,
-            gender,
-            phone_numbers,
-            image_url,
-          } = evt.data;
+        const {
+          id,
+          first_name,
+          last_name,
+          email_addresses,
+          gender,
+          phone_numbers,
+          image_url,
+        } = evt.data;
 
-          //makes sure that the email address i get is actually matches the primary email id
-
-          return this.eventEmiter.emitAsync('user.created', {
-            clerkId: id,
-            firstName: first_name,
-            lastName: last_name,
-            contactNumber: phone_numbers[0].phone_number,
-            email: email_addresses[0].email_address,
-            gender: gender,
-            profilePicture: image_url,
-          } as CreateUserDto)
-            ? `New User ${id} created`
-            : `failed to create new user`;
-        }
+        return this.eventEmiter.emitAsync('user.created', {
+          clerkId: id,
+          firstName: first_name,
+          lastName: last_name,
+          contactNumber: phone_numbers[0]?.phone_number,
+          email: email_addresses[0]?.email_address,
+          gender: Gender[gender],
+          profilePicture: image_url,
+        } as CreateUserDto)
+          ? `New User ${id} created`
+          : `failed to create new user`;
       }
     }
     return `event type: ${evt.type} unsupported`;
