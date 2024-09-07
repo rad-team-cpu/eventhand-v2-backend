08711b8f1b16eefa6a157f9ab8eb190e444ecdc5
delgadoarlee <delgadoarlee@gmail.com>
2024-09-08 03:14:06 +0800

    Add review queries

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -20,12 +20,11 @@ export class ReviewsController {
 
   @Post()
   async create(@Body() createReviewDto: CreateReviewDto): Promise<Review> {
-    console.log(createReviewDto)
+    console.log(createReviewDto);
     try {
       return await this.reviewsService.create(createReviewDto);
-
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
@@ -37,6 +36,20 @@ export class ReviewsController {
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Review> {
     return await this.reviewsService.findOne(id);
+  }
+
+  @Get(':id/list')
+  async getAverageRating(
+    @Param('id') id: string,
+  ): Promise<{ reviews: Review[]; averageRating: number }> {
+    try {
+      const averageRating = await this.reviewsService.getAverageRating(id);
+      const reviews = await this.reviewsService.getVendorReviews(id);
+
+      return { reviews, averageRating };
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   @Patch(':id')
