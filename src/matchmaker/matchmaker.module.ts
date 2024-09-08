73933bc386d312529f136ea2af7666485cffbda7
Jhurds <georgejoseabaygar@gmail.com>
2024-09-08 01:37:56 -0700

    Add stuff

--- src/matchmaker/matchmaker.module.ts
+++ src/matchmaker/matchmaker.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { EventSchema, Event } from 'src/events/entities/event.schema';
 import { Package, PackageSchema } from 'src/packages/entities/package.schema';
 import { Vendor, VendorSchema } from 'src/vendors/entities/vendor.schema';
+import { Tag, TagSchema } from 'src/tags/entities/tag.schema';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { Vendor, VendorSchema } from 'src/vendors/entities/vendor.schema';
       { name: Event.name, schema: EventSchema },
       { name: Package.name, schema: PackageSchema },
       { name: Vendor.name, schema: VendorSchema },
+      { name: Tag.name, schema: TagSchema },
     ]),
   ],
   controllers: [MatchmakerController],
