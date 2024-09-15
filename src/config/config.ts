2975c2d29903af2daf4a677883816a7b5c64d86f
Jhurds <georgejoseabaygar@gmail.com>
2024-09-15 22:48:04 +0800

    Fix database

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ export default () => ({
   database: {
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    mongoConnectionURI: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DATABASE}.pf5unz2.mongodb.net/event-hand-dev?retryWrites=true&w=majority&appName=event-hand-dev`,
+    mongoConnectionURI: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@event-hand-dev.pf5unz2.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority&appName=event-hand-dev`,
   },
   clerk: {
     publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
